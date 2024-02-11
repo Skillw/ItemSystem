b4be6d1eef10e2dbce0b805dae8363e4da4e598a
Glom <glom@skillw.com>
2024-02-12 01:40:11 +0800

    自动推送

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,11 @@ import java.net.URL
 plugins {
     `java-library`
     `maven-publish`
+    signing
     id("io.izzel.taboolib") version "1.56"
     id("org.jetbrains.kotlin.jvm") version "1.5.31"
     id("org.jetbrains.dokka") version "1.5.31"
+    id("io.codearte.nexus-staging") version "0.30.0"
 }
 
 
@@ -112,23 +114,83 @@ configure<JavaPluginConvention> {
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
+tasks.register<Jar>("buildJavadocJar") {
+    dependsOn(tasks.dokkaJavadoc)
+    from(tasks.dokkaJavadoc.flatMap { it.outputDirectory })
+    archiveClassifier.set("javadoc")
+}
+
+tasks.register<Jar>("buildSourcesJar") {
+    dependsOn(JavaPlugin.CLASSES_TASK_NAME)
+    archiveClassifier.set("sources")
+    from(sourceSets["main"].allSource)
+}
+
 publishing {
     repositories {
         maven {
-            url = uri("https://repo.tabooproject.org/repository/releases")
+            url = if (project.version.toString().contains("-SNAPSHOT")) {
+                uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
+            } else {
+                uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            }
             credentials {
-                username = project.findProperty("taboolibUsername").toString()
-                password = project.findProperty("taboolibPassword").toString()
+                username = project.findProperty("username").toString()
+                password = project.findProperty("password").toString()
             }
             authentication {
                 create<BasicAuthentication>("basic")
+
             }
         }
+        mavenLocal()
     }
     publications {
         create<MavenPublication>("library") {
             from(components["java"])
+            artifact(tasks["buildJavadocJar"])
+            artifact(tasks["buildSourcesJar"])
+            version = project.version.toString()
             groupId = project.group.toString()
+            pom {
+                name.set(project.name)
+                description.set("Bukkit Attribute Engine Plugin.")
+                url.set("https://github.com/Glom-c/AttributeSystem/")
+
+                dependencies {
+//                    compileOnly("com.skillw.pouvoir:Pouvoir:1.6.4-8")
+                }
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://github.com/Glom-c/AttributeSystem/blob/main/LICENSE")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("Skillw")
+                        name.set("Glom_")
+                        email.set("glom@skillw.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git:https://github.com/Glom-c/AttributeSystem.git")
+                    developerConnection.set("scm:git:ssh:https://github.com/Glom-c/AttributeSystem.git")
+                    url.set("https://github.com/Glom-c/AttributeSystem.git")
+                }
+            }
         }
     }
+}
+
+nexusStaging {
+    serverUrl = "https://s01.oss.sonatype.org/service/local/"
+    username = project.findProperty("username").toString()
+    password = project.findProperty("password").toString()
+    packageGroup = "com.skillw"
+}
+
+signing {
+    sign(publishing.publications.getAt("library"))
 }
